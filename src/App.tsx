56b124a5fd59e07dca0a41b85361dfc1e9578460
[PeterWolf242] <[peter.ebm@web.de]>
2025-02-07 11:27:11 +0100

    Design Update V2.0

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import AnimalCard from "./components/AnimalCard";
 function App() {
 	return (
 		<div className="w-full h-full bg-creme pb-24">
-			<h1 className="text-yellow-creme text-6xl uppercase tracking-[10px] font-bold text-center pt-10 [text-shadow:_0_4px_14px_rgb(0_0_0_/_0.8)]">
+			<h1 className="text-yellow-creme text-6xl uppercase tracking-[10px] font-bold text-center mt-32 mb-75 [text-shadow:_0_4px_14px_rgb(0_0_0_/_0.8)]">
 				ZooPartyPlus
 			</h1>
 			<div className="w-full grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-10 mt-12 mx-auto px-4">
